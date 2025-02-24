d81aacb641b234af690688a6ddf07ec9a7bd8934
gnipka <gnipka29@gmail.com>
2025-02-24 19:30:49 +0300

    выполнена Практика А в 1 урок

---
+++ Lesson1/ReadySolution/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ReadySolution;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Практика А
+
+        // Создайте программу, которая объявляет различные переменные разных типов данных 
+        // (целочисленный, вещественный, строковый и логический) и выводит их значения на экран.  
+
+        int age = 20; 
+        Console.WriteLine($"Age: {age}");
+
+        double pi = 3.14;
+        Console.WriteLine($"Pi: {pi}");
+
+        string name = "John";
+        Console.WriteLine($"Name: {name}");
+        
+        bool isMale = false;
+        Console.WriteLine($"IsMale: {isMale}");
+
+        // Напишите программу, которая принимает два числа от пользователя и выполняет арифметические операции (сложение, вычитание, умножение, деление) 
+        // над ними, выводя результат каждой операции на экран.
+        
+        Console.WriteLine("Введите первое число:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2;
+        Console.WriteLine($"Сумма: {sum}");
+
+        int diff = number1 - number2;
+        Console.WriteLine($"Разность: {diff}");
+
+        // Умножение *
+        // Деление /
+
+        // Напишите программу, которая принимает строку от пользователя, содержащую число, и конвертирует его в целое число. 
+        // Затем увеличьте это число на 5 и выведите результат.
+        
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int total = number + 5;
+        Console.WriteLine($"Total: {total}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Code10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // WriteLine - вывод в консоль
+        Console.WriteLine("Hello, World!");
+
+        // ReadLine - ввод с клавиатуры
+        Console.ReadLine();
+
+        int age = 20;
+        string name = "John";
+        double pi = 3.14;
+        bool isMale = false;
+        
+    }
+}
