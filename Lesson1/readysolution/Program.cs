4b8879e1906a13bd34808e692da53bba6f551672
Ivan-Pantyukhin <i_pantyukhin@inbox.ru>
2025-03-03 20:54:04 +0500

    выполнены практики урока

--- Lesson1/readysolution/Program.cs
+++ Lesson1/readysolution/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿namespace ReadySolution;
+using System;
 
-internal class Program
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        int number2 = 0;
-
         // Практика А
 
-        // Объявление переменных разных типов данных и вывод их значений
-        int age = 20;
+        // Создайте программу, которая объявляет различные переменные разных типов данных 
+        // (целочисленный, вещественный, строковый и логический) и выводит их значения на экран.  
+
+        int age = 20; 
         Console.WriteLine($"Age: {age}");
 
         double pi = 3.14;
@@ -17,126 +18,33 @@ internal class Program
 
         string name = "John";
         Console.WriteLine($"Name: {name}");
-
+        
         bool isMale = false;
-        Console.WriteLine($"Is Male: {isMale}");
+        Console.WriteLine($"IsMale: {isMale}");
 
-
-        // Арифметические операции с двумя числами
+        // Напишите программу, которая принимает два числа от пользователя и выполняет арифметические операции (сложение, вычитание, умножение, деление) 
+        // над ними, выводя результат каждой операции на экран.
+        
         Console.WriteLine("Введите первое число:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
 
-    ;
+        int sum = number1 + number2;
+        Console.WriteLine($"Сумма: {sum}");
 
-        if (int.TryParse(Console.ReadLine(), out int number1))
-        {
-            Console.WriteLine("Введите второе число:");
-            if (!int.TryParse(Console.ReadLine(), out int number2))
-            {
-                Console.WriteLine("Некорректный ввод второго числа.");
-            }
-            else
-            {
-                int sum = number1 + number2;
-                Console.WriteLine($"Сумма: {sum}");
+        int diff = number1 - number2;
+        Console.WriteLine($"Разность: {diff}");
 
-                int diff = number1 - number2;
-                Console.WriteLine($"Разность: {diff}");
+        // Умножение *
+        // Деление /
 
-                int mult = number1 * number2;
-                Console.WriteLine($"Произведение: {mult}");
-
-                if (number2 != 0) //Проверка на деление на ноль
-                {
-                    double division = (double)number1 / number2; // Преобразование в double для точного результата
-                    Console.WriteLine($"Частное: {division}");
-                }
-                else
-                {
-                    Console.WriteLine("Деление на ноль невозможно!");
-                }
-            }
-        }
-        else
-        {
-            Console.WriteLine("Некорректный ввод первого числа.");
-        }
-
-
-        // Условие с проверкой на дождь
-        Console.WriteLine("На улице идет дождь? (да/нет)");
-#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
-        string answer = Console.ReadLine().ToLower(); // Преобразование в нижний регистр для большей точности
-#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
-        if (answer == "да")
-        {
-            Console.WriteLine("Не забудьте зонт!");
-        }
-        else
-        {
-            Console.WriteLine("Можно без зонта!");
-        }
-
-
-        // Цикл for
-        Console.WriteLine("Цикл for:");
-        for (int i = 0; i < 5; i++)
-        {
-            Console.WriteLine(i); // 0 1 2 3 4
-        }
-
-
-        // Сравнение двух чисел
-        Console.WriteLine("Введите первое число для сравнения:");
-        if (int.TryParse(Console.ReadLine(), out number1))
-        {
-            Console.WriteLine("Введите второе число для сравнения:");
-            if (int.TryParse(Console.ReadLine(), out number2))
-            {
-                if (number1 < number2)
-                {
-                    Console.WriteLine($"Меньшее число: {number1}");
-                    Console.WriteLine($"Большее число: {number2}");
-                }
-                else if (number1 > number2)
-                {
-                    Console.WriteLine($"Меньшее число: {number2}");
-                    Console.WriteLine($"Большее число: {number1}");
-                }
-                else
-                {
-                    Console.WriteLine("Числа равны");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Некорректный ввод второго числа.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Некорректный ввод первого числа.");
-        }
-
-
-        // Практика В
-        Master(); //Вызов функции master
-
-        Console.ReadKey(); //Задержка консоли для просмотра результатов
-
-        void Master()
-        {
-            Console.WriteLine("Введите ваше имя: ");
-            Console.WriteLine("Здравствуйте, " + Console.ReadLine() + "! Введите год вашего рождения:");
-
-            if (int.TryParse(Console.ReadLine(), out int birthYear))
-            {
-                int age = DateTime.Now.Year - birthYear;
-                Console.WriteLine($"{age} - Ваш возраст");
-            }
-            else
-            {
-                Console.WriteLine("Некорректный ввод года рождения.");
-            }
-        }
+        // Напишите программу, которая принимает строку от пользователя, содержащую число, и конвертирует его в целое число. 
+        // Затем увеличьте это число на 5 и выведите результат.
+        
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int total = number + 5;
+        Console.WriteLine($"Total: {total}");
     }
 }
---
+++ Lesson2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Arrays
+{
+    public static void Main(string[] args)
+    {
+        // Практика А
+
+        // 1. Объявление массива целых чисел длины 5
+        int[] intArray = new int[5];
+
+        // 2. Инициализация массива из трех строк
+        string[] stringArray = { "Строка 1", "Строка 2", "Строка 3" };
+
+
+        // Практика B
+
+        // 3. Доступ к элементам массива
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Второй элемент: " + numbers[1]); // Вывод второго элемента (индекс 1)
+        numbers[2] = 100; // Изменение третьего элемента (индекс 2)
+        Console.WriteLine("Третий элемент после изменения: " + numbers[2]);
+
+        // 4. Длина массива
+        Console.WriteLine("Длина массива numbers: " + numbers.Length);
+
+
+        // Практика C
+
+        // 5. Объявление и инициализация пустого массива длины 4
+        int[] emptyArray = new int[4]; // Инициализация нулями
+
+
+        // 6. Создание массива с помощью оператора new (аналогично пункту 1, new в C# используется при объявлении)
+        int[] newArray = new int[5];
+        newArray[0] = 1;
+        newArray[1] = 2;
+        newArray[2] = 3;
+        newArray[3] = 4;
+        newArray[4] = 5;
+
+
+        // 7. Инициализация массива символов
+        char[] charArray = { 'A', 'B', 'C' };
+
+
+        // 8. Заполнение массива данными вручную (аналогично пункту 6)
+        int[] manualArray = new int[5];
+        manualArray[0] = 10;
+        manualArray[1] = 20;
+        manualArray[2] = 30;
+        manualArray[3] = 40;
+        manualArray[4] = 50;
+
+
+        Console.ReadKey(); // Задержка консоли для просмотра результатов
+    }
+}
